91662547da553115464e7d520e79215be1a1f97f
Timóteo Ramos <timoteoramos@ymail.com>
2025-12-28 06:36:26 -0300

    feat: payment methods

--- src/agnescafe/apps/finnancial/admin.py
+++ src/agnescafe/apps/finnancial/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import CashFlow
+from .models import CashFlow, Payment, PaymentMethod
+
+
+class PaymentInline(admin.StackedInline):
+    exclude = ["is_removed"]
+    model = Payment
+    extra = 1
 
 
 @admin.register(CashFlow)
 class CashFlowAdmin(admin.ModelAdmin):
     exclude = ["is_removed"]
+    inlines = [PaymentInline]
     list_display = ("description", "input", "total", "created",)
     list_filter = ("input", "created",)
     search_fields = ("description",)
     ordering = ("-created",)
+
+
+@admin.register(PaymentMethod)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    exclude = ["is_removed"]
+    list_display = ("name", "created",)
+    search_fields = ("name",)
+    ordering = ("name",)
--- src/agnescafe/apps/finnancial/models.py
+++ src/agnescafe/apps/finnancial/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from model_utils.models import SoftDeletableModel, TimeStampedModel
 
 
+class PaymentMethod(SoftDeletableModel, TimeStampedModel):
+    name = models.CharField(
+        max_length=32,
+        verbose_name="nome",
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "método de pagamento"
+        verbose_name_plural = "métodos de pagamento"
+
+
 class CashFlow(SoftDeletableModel, TimeStampedModel):
     description = models.CharField(
         max_length=64,
@@ -11,6 +25,12 @@ class CashFlow(SoftDeletableModel, TimeStampedModel):
     input = models.BooleanField(
         default=True,
         verbose_name="entrada",
+    )
+
+    payment_methods = models.ManyToManyField(
+        PaymentMethod,
+        through="Payment",
+        verbose_name="métodos de pagamento",
     )
 
     total = models.DecimalField(
@@ -25,3 +45,30 @@ class CashFlow(SoftDeletableModel, TimeStampedModel):
     class Meta:
         verbose_name = "fluxo de caixa"
         verbose_name_plural = "fluxos de caixa"
+
+
+class Payment(SoftDeletableModel, TimeStampedModel):
+    cash_flow = models.ForeignKey(
+        CashFlow,
+        on_delete=models.CASCADE,
+        verbose_name="fluxo de Caixa",
+    )
+
+    method = models.ForeignKey(
+        PaymentMethod,
+        on_delete=models.CASCADE,
+        verbose_name="método de pagamento",
+    )
+
+    amount = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        verbose_name="valor",
+    )
+
+    def __str__(self):
+        return f"{self.method.name} - R$ {self.amount}"
+
+    class Meta:
+        verbose_name = "pagamento"
+        verbose_name_plural = "pagamentos"
